749e039ad0d40627ec1ec68408c8ec06a587f013
koitatu <koitatu@gmail.com>
2019-08-14 20:04:20 +0900

    Create attach_file_slack.sh

---
+++ attach_file_slack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Set Env
+MESSAGE="$@"
+TOKEN="<MY_TOKEN>"
+CHANNEL="#<MY_CHANNEL>"
+DATE_TIME=$(date '+%Y%m%d %H:%M')
+URL="https://slack.com/api/files.upload"
+
+# Send to slack
+curl -F content="${MESSAGE}" -F channels="${CHANNEL}" -F token=${TOKEN} -F title="${DATE_TIME}" -F filename="${DATE_TIME}" -F initial_comment="result ${DATE_TIME}" 
