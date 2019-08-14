08208e1f3b3afcbc15ff903fe22e6b367dc39c46
koitatu <koitatu@gmail.com>
2019-08-14 20:37:43 +0900

    <!here>メンション追加

--- attach_file_slack.sh
+++ attach_file_slack.sh
@@ -8,4 +8,4 @@ DATE_TIME=$(date '+%Y%m%d %H:%M')
 URL="https://slack.com/api/files.upload"
 
 # Send to slack
-curl -F content="${MESSAGE}" -F channels="${CHANNEL}" -F token=${TOKEN} -F title="${DATE_TIME}" -F filename="${DATE_TIME}" -F initial_comment="result ${DATE_TIME}" 
+curl -F content="${MESSAGE}" -F channels="${CHANNEL}" -F token=${TOKEN} -F title="${DATE_TIME}" -F filename="${DATE_TIME}" -F initial_comment=" <!here> result ${DATE_TIME}" 
